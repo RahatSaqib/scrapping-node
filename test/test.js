63223ed57b32582ae77b05f971598fe05cdcd85e
Rahat Saqib <37343956+RahatSaqib@users.noreply.github.com>
2022-07-20 22:02:37 +0600

    updated test.js for testing

--- test/test.js
+++ test/test.js
@@ -1,10 +1,10 @@
 const express = require('express');
 const chai = require('chai');
-const request = require('supertest');
+const server = require('supertest');
 const app = express();
 var should = require("should");
-var server = request
-// var server = request.agent("http://localhost:8443");
+// var server = server.agent("http://localhost:8443");
+var server = server(app)
 describe('Get Next Page URL', () => {
     it('should get next url', (done) => {
         server
@@ -21,22 +21,20 @@ describe('Get Next Page URL', () => {
     });
  });
 });
-describe('Scrape  URL', () => {
-    it('should Scrape truck item', (done) => {
-        return new Promise((resolve,reject)=>{
-            server
-            .get("/scrape-truck-item")
-            .expect("Content-type",/json/)
-            .expect(200) // THis is HTTP response
-            .end(function(err,res){
-              // HTTP status should be 200
-              res.status.should.equal(200);
-            //   // Error key should be false.
-              res.error.should.equal(false);
-              resolve (done());
-             // more validations can be added here as required
-        })
+// describe('Scrape  URL', () => {
+//     it('should Scrape truck item', (done) => {
+//             server
+//             .get("/scrape-truck-item")
+//             .expect("Content-type",/json/)
+//             .expect(200) // THis is HTTP response
+//             .end(function(err,res){
+//               // HTTP status should be 200
+//               res.status.should.equal(200);
+//             //   // Error key should be false.
+//               res.error.should.equal(false);
+//               done();
+//              // more validations can be added here as required
+//         })
 
-    });
- });
-});
+//  });
+// });
