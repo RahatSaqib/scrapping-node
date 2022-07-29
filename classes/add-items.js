e15680a05903a45059b33a6e70ad6b061d98208b
Rahat Saqib <37343956+RahatSaqib@users.noreply.github.com>
2022-07-29 11:12:31 +0600

    ADDED RETRY STRETEGIES TO SCARPE ITEM

--- classes/add-items.js
+++ classes/add-items.js
@@ -3,17 +3,6 @@ const cheerio = require("cheerio");
 const fs = require("fs");
 const axiosRetry = require('axios-retry');
 
-axiosRetry(axios, {
-  retries: 5, // number of retries
-  retryDelay: (retryCount) => {
-    console.log(`retry attempt: ${retryCount}`);
-    return retryCount * 2000; // time interval between retries
-  },
-  retryCondition: (error) => {
-    // if retry condition is not specified, by default idempotent requests are retried
-    return error.response.status === 503;
-  },
-});
 module.exports = class ClassAddItems {
   constructor(...args) {
     this.path = "add-items";
--- classes/scrape-truck-item.js
+++ classes/scrape-truck-item.js
@@ -1,24 +1,12 @@
 const axios = require("axios");
 const cheerio = require("cheerio");
 const fs = require("fs");
-const axiosRetry = require('axios-retry');
-
-axiosRetry(axios, {
-  retries: 5, // number of retries
-  retryDelay: (retryCount) => {
-    console.log(`retry attempt: ${retryCount}`);
-    return retryCount * 2000; // time interval between retries
-  },
-  retryCondition: (error) => {
-    // if retry condition is not specified, by default idempotent requests are retried
-    return error.response.status === 503;
-  },
-});
+const axiosRetry = require("axios-retry");
 module.exports = class ClassScrapeTruckItem {
   constructor(...args) {
     this.path = "scrape-truck-item";
     this.truckItems = {
-      title:'',
+      title: "",
       price: "",
       registration_date: "",
       production_date: "",
@@ -27,51 +15,69 @@ module.exports = class ClassScrapeTruckItem {
     };
   }
   async fetchTruckData(url) {
+    // retryWrapper(axios, { retry_time: 3, retry_status_code: 404 });
     return new Promise(async (resolve, reject) => {
-      await axios
-        .get(url)
-        .then((response) => {
-          const truck$ = cheerio.load(response.data);
-          // console.log(truck$.html())
-          let title = truck$('#siteWrap > main > section > div.offer-header__row.hidden-xs.visible-tablet-up > h1')
-          let registration_date = truck$(
-            "#parameters > ul:nth-child(2) > li:nth-child(3)"
-          ); //get all element with name article
-          let production_date = truck$("div.offer-header__row.visible-xs > span > span:nth-child(1)"); //get all element with nth-child for production date 
-          let mileage = truck$("div.offer-header__row.visible-xs > span > span:nth-child(2)"); //get all element with nth-child for production date 
-          let currency = truck$('#siteWrap > main > section > div.offer-header__row.hidden-xs.visible-tablet-up > div.offer-price > div.wrapper > span > span')
-          currency = currency.text()
-          let price = truck$(".offer-price").attr("data-price");
-          let power = truck$("#parameters > ul:nth-child(1) > li:nth-child(8)"); //get all element with nth-child for power 
+      let retries = 0;
+      let success = false;
+      const maxRetries = 3;
+      while (retries < maxRetries && !success) {
+        try {
+          await axios.get(url).then((response) => {
+            const truck$ = cheerio.load(response.data);
+            // console.log(truck$.html())
+            let title = truck$(
+              "#siteWrap > main > section > div.offer-header__row.hidden-xs.visible-tablet-up > h1"
+            );
+            let registration_date = truck$(
+              "#parameters > ul:nth-child(2) > li:nth-child(3)"
+            ); //get all element with name article
+            let production_date = truck$(
+              "div.offer-header__row.visible-xs > span > span:nth-child(1)"
+            ); //get all element with nth-child for production date
+            let mileage = truck$(
+              "div.offer-header__row.visible-xs > span > span:nth-child(2)"
+            ); //get all element with nth-child for production date
+            let currency = truck$(
+              "#siteWrap > main > section > div.offer-header__row.hidden-xs.visible-tablet-up > div.offer-price > div.wrapper > span > span"
+            );
+            currency = currency.text();
+            let price = truck$(".offer-price").attr("data-price");
+            let power = truck$(
+              "#parameters > ul:nth-child(1) > li:nth-child(8)"
+            ); //get all element with nth-child for power
 
-          
-          let text = registration_date.find("span").text();
-          title = title.text().replace(/^\s+|\s+$/g ,'')
-          registration_date = registration_date.text().replace(/[^\d/]/g, "");
-          power = power.text().replace(/[^\d]/g, "") + "hp";
-          production_date = production_date.text().replace(/[^\d/]/g, "");
-          mileage = mileage.text().replace(/[^\dkm]/g, "");
-          price = price.replace(' ', ",");
+            let text = registration_date.find("span").text();
+            title = title.text().replace(/^\s+|\s+$/g, "");
+            registration_date = registration_date.text().replace(/[^\d/]/g, "");
+            power = power.text().replace(/[^\d]/g, "") + "hp";
+            production_date = production_date.text().replace(/[^\d/]/g, "");
+            mileage = mileage.text().replace(/[^\dkm]/g, "");
+            price = price.replace(" ", ",");
 
-          this.truckItems.title = title
-          this.truckItems.price = price + ' ' + currency;
-          this.truckItems.power = power;
-          this.truckItems.production_date = production_date;
-          this.truckItems.mileage = mileage;
+            this.truckItems.title = title;
+            this.truckItems.price = price + " " + currency;
+            this.truckItems.power = power;
+            this.truckItems.production_date = production_date;
+            this.truckItems.mileage = mileage;
 
-          if (text == "Pierwsza rejestracja" || text == "First Registration") {
-            this.truckItems.registration_date = registration_date;
-          }
-
-          resolve(this.truckItems);
-        })
-        .catch((error) => {
-          // console.log(error.message ,error.statusCode)
-          // reject(error)
-          resolve({
-            status: "404 NOT FOUND",
+            if (
+              text == "Pierwsza rejestracja" ||
+              text == "First Registration"
+            ) {
+              this.truckItems.registration_date = registration_date;
+            }
+            success = true;
+            resolve(this.truckItems);
           });
-        });
+        } catch (err) {
+          console.log('error' , retries)
+          retries++;
+        }
+      }
+
+      resolve({
+        status: "404 NOT FOUND",
+      });
     });
   }
   handle(e) {
